7edd25a44084be226db03f59c1981f7d855e0182
Makoto Inoue <2630+makoto@users.noreply.github.com>
2022-01-31 16:38:16 +0000

    Add text into client example

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -83,6 +83,12 @@ const provider = new CCIPReadProvider(baseProvider);
     const responseData = await resolver.resolve(dnsName(name), data);
     const addr = Resolver.decodeFunctionResult('addr(bytes32)', responseData);
     console.log(`${name}: ${addr}`);
+
+    const textData = Resolver.encodeFunctionData('text(bytes32,string)', [node, 'description']);
+    const textResponseData = await resolver.resolve(dnsName(name), textData);
+    const value = Resolver.decodeFunctionResult('text(bytes32,string)', textResponseData);
+    console.log(`${name}: ${value}`);
+
   } catch (e) {
     console.log(e);
   }
