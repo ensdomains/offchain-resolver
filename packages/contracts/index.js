5b933785b6ae5fa5433d9caa4eb414596f7aea43
Makoto Inoue <2630+makoto@users.noreply.github.com>
2022-02-28 10:08:21 +0000

    rm index.js

--- packages/contracts/index.js
+++
@@ -1,7 +0,0 @@
-const IExtendedResolver = require('./artifacts/contracts/IExtendedResolver.sol/IExtendedResolver.json')
-const OffchainResolver = require('./artifacts/contracts/OffchainResolver.sol/OffchainResolver.json')
-
-module.exports = {
-  IExtendedResolver,
-  OffchainResolver
-}
