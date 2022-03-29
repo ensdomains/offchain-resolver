aed2da02fcc1c44effaf40f8bbb99110b97e7e2f
Makoto Inoue <2630+makoto@users.noreply.github.com>
2022-03-29 21:47:10 +0100

    yarn lint --fix

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -6,19 +6,25 @@ program
   .requiredOption('-r --registry <address>', 'ENS registry address')
   .option('-p --provider <url>', 'web3 provider URL', 'http://localhost:8545/')
   .option('-i --chainId <chainId>', 'chainId', '31337')
+  .option('-n --name <name>', 'name', 'unknown')
   .argument('<name>');
 
 program.parse(process.argv);
 const options = program.opts();
-const ensAddress = options.registry
-const chainId = parseInt(options.chainId)
-const provider = new ethers.providers.JsonRpcProvider(options.provider, { chainId, name: 'unknown', ensAddress });
+const ensAddress = options.registry;
+const chainId = parseInt(options.chainId);
+const name = options.name;
+const provider = new ethers.providers.JsonRpcProvider(options.provider, {
+  chainId,
+  name,
+  ensAddress,
+});
 (async () => {
   const name = program.args[0];
-  let resolver = await provider.getResolver(name)
-  let resolveName = await provider.resolveName(name)
-  if(resolver){
-    console.log(`resolver address ${resolver.address}`)
-    console.log(`eth address ${resolveName}`)  
+  let resolver = await provider.getResolver(name);
+  let resolveName = await provider.resolveName(name);
+  if (resolver) {
+    console.log(`resolver address ${resolver.address}`);
+    console.log(`eth address ${resolveName}`);
   }
 })();
