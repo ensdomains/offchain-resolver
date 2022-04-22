0de1f2377c78493a0abe6d17796b9eb33af6a64b
Makoto Inoue <2630+makoto@users.noreply.github.com>
2022-04-22 17:57:56 +0100

    Simplify multicoin call

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,6 +1,6 @@
 import { Command } from 'commander';
-import ethers, { utils, BigNumber } from 'ethers';
-import { formatsByCoinType } from '@ensdomains/address-encoder';
+import ethers from 'ethers';
+// import { formatsByCoinType } from '@ensdomains/address-encoder';
 
 const program = new Command();
 program
@@ -23,21 +23,11 @@ const provider = new ethers.providers.JsonRpcProvider(options.provider, {
 (async () => {
   const name = program.args[0];
   let resolver = await provider.getResolver(name);
-  let resolveName = await provider.resolveName(name);
-  let btcAddress;
-  let coinType = 0;
   if (resolver) {
-    const encodedCoinType = utils.hexZeroPad(
-      BigNumber.from(coinType).toHexString(),
-      32
-    );
-    const btcData = await resolver._fetchBytes('0xf1cb7e06', encodedCoinType);
-    if (btcData) {
-      let buffer = Buffer.from(btcData.slice(2), 'hex');
-      btcAddress = formatsByCoinType[coinType].encoder(buffer);
-    }
+    let ethAddress = await resolver.getAddress();
+    let btcAddress = await resolver.getAddress(0);
     console.log(`resolver address ${resolver.address}`);
-    console.log(`eth address ${resolveName}`);
+    console.log(`eth address ${ethAddress}`);
     console.log(`btc address ${btcAddress}`);
   } else {
     console.log('no resolver found');
